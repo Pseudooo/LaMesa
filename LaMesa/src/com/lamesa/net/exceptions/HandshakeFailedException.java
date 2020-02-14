125e5b13660ec98e28667be8cc197dbab8c09da5
pseudo <Mitchell.Wright877@gmail.com>
2020-02-14 12:30:03 +0000

    Reworked network system and now have a handshake system

---
+++ LaMesa/src/com/lamesa/net/exceptions/HandshakeFailedException.java
@@ -0,0 +1,10 @@
+package com.lamesa.net.exceptions;
+
+public class HandshakeFailedException extends Exception {
+	private static final long serialVersionUID = -9041337088906020415L;
+
+	public HandshakeFailedException(String msg) {
+		super(msg);
+	}
+	
+}

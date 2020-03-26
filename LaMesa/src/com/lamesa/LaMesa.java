fabac91ad48865cca7fa4faf4f21216c200a981c
UP898548 <56345902+UP898548@users.noreply.github.com>
2020-03-26 20:28:48 +0000

    Add files via upload

--- LaMesa/src/com/lamesa/LaMesa.java
+++ LaMesa/src/com/lamesa/LaMesa.java
@@ -4,20 +4,40 @@ package LaMesa;
 	
 	public static void main(String[] args) {
             
+            //All Menu items and prices are stored in separate ArrayLists (Items, Prices, Items, Prices) etc.
+            
             Restaurant testR = new Restaurant();
+            
+            //addNewRestaurant adds two blank ArrayLists to the end of the restMenus ArrayList, which is where bith Prices and Items are stored
             testR.addNewRestaurant();
             testR.addNewRestaurant();
             
+            //addMenuItems and addMenuPrices are used to add items to both the menu and prices ArrayLists.
+            //An index number must be supplied but it is coded to accept 0 as first and 1 is second etc. (EACH RESTAURANT USES THE SAME NUMBER FOR BOTH MENUS AND PRICES) 
             testR.addMenuItems(0, "Test 1");
             testR.addMenuItems(1, "Test 2");
             testR.addMenuPrices(0, 1.1);
             testR.addMenuPrices(0, 2.2);
             testR.addMenuPrices(1, 3.3);
 
-            System.out.println(testR.returnMenuItems(0));
+            //Print statements to demonstrate code
+            System.out.println("---Menu Price and Item Return Example--- \n" + testR.returnMenuItems(0));
             System.out.println(testR.returnMenuPrices(0));
             System.out.println(testR.returnMenuItems(1));
             System.out.println(testR.returnMenuPrices(1));
+            
+            //addRestaurantInfo adds Restaurant Name, current seats and maximum seats to their own arraylist in the set order
+            testR.addRestaurantInformation("Example Name", 100, 200);
+            testR.addRestaurantInformation("Example Name 2", 10, 1000);
+
+            //Demonstration of the code used to retrieve elements from the ArrayList where, like before, each restaurant in order uses the index 0,1,2 etc.
+            System.out.println("---Restaurant Info Example--- \n" + testR.getRestaurantInfo(0));
+            System.out.println(testR.getRestaurantName(0));
+            System.out.println(testR.getRestaurantCurrSeats(0));
+            System.out.println(testR.getRestaurantMaxSeats(0));
+            System.out.println(testR.getRestaurantName(1));
+            System.out.println(testR.getRestaurantCurrSeats(1));
+            System.out.println(testR.getRestaurantMaxSeats(1));
         }
 	
 }
--- LaMesa/src/com/lamesa/Restaurant.java
+++ LaMesa/src/com/lamesa/Restaurant.java
@@ -5,17 +5,120 @@ import java.util.ArrayList;
 
 public class Restaurant {
     
-    public String restaurantName;
     ArrayList<String> menuItems;
     ArrayList<Double> menuPrices;
     ArrayList<ArrayList> restMenus;
+    ArrayList<Object> restInfo;
+    String restName;
+    int currSeats;
+    int maxSeats;
     
     public Restaurant(){
         menuItems = new ArrayList<String>();
         menuPrices = new ArrayList<Double>();
         restMenus = new ArrayList<ArrayList>();
+        restInfo = new ArrayList<Object>();
     }
     
+    public String getRestInfoArray(){
+        return restInfo.toString();
+    }
+    
+    //Below is the Restaurant Information
+    
+    /**
+     * Allows input of restaurant Name, current Seats and Maximum Seats and adds them to an ArrayList
+     * @param newRestName
+     * @param newCurrSeats
+     * @param newMaxSeats 
+     */
+    public void addRestaurantInformation(String newRestName, int newCurrSeats, int newMaxSeats){
+        restName = newRestName;
+        currSeats = newCurrSeats;
+        maxSeats = newMaxSeats;
+        restInfo.add(restName);
+        restInfo.add(currSeats);
+        restInfo.add(maxSeats);
+    }
+    
+    /**
+     * Retrieves the entire ArrayList with all values in a string
+     * @param listIndex
+     * @return 
+     */
+    public String getRestaurantInfo(int listIndex){
+        
+        String stringList = "";
+        
+        for(Object s : restInfo){
+            stringList += s + "\t";
+        }
+        
+        return stringList;
+    }
+    
+    /**
+     * Returns a string of a Restaurant Name dependant upon the index number placed in
+     * @param listIndex
+     * @return 
+     */
+    public String getRestaurantName(int listIndex){
+        int index = 3*listIndex;
+        String stringList = "";
+        
+        if(listIndex <= 0){
+            index = 0;
+        }
+                
+        stringList = "" + restInfo.get(index);
+        
+        return stringList;
+    }
+        
+    /**
+     * Returns a string of the Current Number of Seats dependant upon the index number placed in
+     * @param listIndex
+     * @return 
+     */
+    public String getRestaurantCurrSeats(int listIndex){
+        int index = 3*listIndex + 1;
+        String stringList = "";
+        
+        if(listIndex <= 0){
+            index = 1;
+        }
+        
+        stringList = "" + restInfo.get(index);
+        
+        return stringList;
+    }
+        
+    /**
+     * Returns a string of the Maximum Number of Seats dependant upon the index number placed in
+     * @param listIndex
+     * @return 
+     */
+    public String getRestaurantMaxSeats(int listIndex){
+        int index = 3*listIndex + 2;
+        String stringList = "";
+        
+        if(listIndex <= 0){
+            index = 2;
+        }
+        
+        stringList = "" + restInfo.get(index);
+
+        return stringList;
+    }
+    
+    
+    //Below is the Menu Gets and Sets
+    
+    /**
+     * Adds two blank ArrayLists to the main ArrayList (restMenus)
+     * 
+     * SHOULD RUN EVERYTIME A RESTAURANT IS ADDED TO SET UP THE ARRAYLISTS
+     */
     public void addNewRestaurant(){
         
         ArrayList<String> newMenuItems = new ArrayList<String>();
@@ -25,6 +128,11 @@ public class Restaurant {
         restMenus.add(newMenuPrices);
     }
     
+    /**
+     * Adds a String of a Menu Item to the menuItems ArrayList
+     * @param listIndex
+     * @param newMenuItem 
+     */
     public void addMenuItems(int listIndex, String newMenuItem){
         
         int index = 2*listIndex;
@@ -36,7 +144,12 @@ public class Restaurant {
         restMenus.get(index).add(newMenuItem);
         
     }
-    
+
+    /**
+     * Adds a Double of a Menu Item Price to the menuPrices ArrayList
+     * @param listIndex
+     * @param newMenuPrice 
+     */
     public void addMenuPrices(int listIndex, double newMenuPrice){
                 
         int index = 2*listIndex + 1;
@@ -49,13 +162,11 @@ public class Restaurant {
     }
     
     
-    
-    
-    
-    
-    // Need to add ability to search through main list
-    
-    
+    /**
+     * Returns a String of all Items within an Indexed ArrayList
+     * @param listIndex
+     * @return 
+     */
     public String returnMenuItems(int listIndex){
         
         int index = 2*listIndex;
@@ -72,6 +183,11 @@ public class Restaurant {
         return listString;
     }
     
+    /**
+     * Returns a String of all Prices within an Indexed ArrayList
+     * @param listIndex
+     * @return 
+     */
     public String returnMenuPrices(int listIndex){
         
         int index = 2*listIndex + 1;
@@ -84,7 +200,31 @@ public class Restaurant {
         return listString;
     }
     
-    public int getListLen(){
+    public int getRestInfoLen(){
+        return restInfo.size();
+    }
+    
+    public int getRestMenusLen(){
         return restMenus.size();
+    }
+    
+    public int getMenuItemsLen(int listIndex){
+        
+        int index = 2*listIndex;
+        
+        if (index<=0){
+            index = 0;
+        }
+        return restMenus.get(index).size();
+    }
+    
+    public int getMenuPricesLen(int listIndex){
+        int index = 2*listIndex + 1;
+        
+        if (index<=0){
+            index = 0;
+        }
+        
+        return restMenus.get(index).size();
     }
 }
